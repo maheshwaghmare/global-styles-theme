3bf9a50b31f8469723ab7808a0012ea56502d283
Andrés <nosolosw@users.noreply.github.com>
2020-04-06 17:53:19 +0200

    Theme demo: try font size implicit attribute (#4)
    * Use var in block
    
    * Add comments to readme
    
    * Show content

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # global-styles-theme
+
+This repo is only used as a demo playground for some of PRs I'm working on.
+
+I only use the branches as an acompanying tool, so at any given point in time, the `master` branch can be broken.
--- index.php
+++ index.php
@@ -10,33 +10,17 @@
 </head>
 
 <body>
-	<!-- wp:heading {"level":1} -->
-	<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-	<!-- /wp:heading -->
-
-	<!-- wp:heading {"level":2} -->
-	<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-	<!-- /wp:heading -->
-	<!-- wp:heading {"level":3} -->
-	<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-	<!-- /wp:heading -->
-
-	<!-- wp:heading {"level":4} -->
-	<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
-	<!-- /wp:heading -->
-
-	<!-- wp:heading {"level":5} -->
-	<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
-	<!-- /wp:heading -->
-
-	<!-- wp:heading {"level":6} -->
-	<h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h6>
-	<!-- /wp:heading -->
-
-	<!-- wp:paragraph -->
-	<p>Mauris consequat augue quis risus sollicitudin commodo. In hac habitasse platea dictumst. Pellentesque massa sem,
-		ultrices non tempus ac, aliquet ac nunc. Sed porttitor ac mi a malesuada. Donec blandit vel arcu blandit
-		scelerisque. Etiam euismod blandit leo a maximus.</p>
-	<!-- /wp:paragraph -->
-
+	<main id="site-content" role="main">
+	<?php
+		while( have_posts() ) {
+			the_post();
+			?>
+			<article>
+				<header><?php the_title('<h1>', '</h1>') ?></header>
+				<section><?php the_content() ?> </section>
+			</article>
+			<?php
+		}
+	?>
+	</main>
 </body>
