09b228e566851aac3cc71d1de8f0ca360a96c23c
Andrés <nosolosw@users.noreply.github.com>
2020-05-07 11:20:17 +0200

    Declare theme support for color, gradient, and font size presets (#6)
    * Declare theme support for color, gradient, and font size presets
    
    * Update vars in use

--- functions.php
+++ functions.php
@@ -15,3 +15,46 @@ function global_styles_experimental_wp_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'global_styles_experimental_wp_enqueue_scripts' );
+
+
+function global_styles_theme_after_setup() {
+	add_theme_support( 'editor-color-palette', array(
+		array(
+			'name'  => __( 'Primary', 'global-styles' ),
+			'slug'  => 'primary',
+			'color' => '#0073AA',
+		),
+		array(
+			'name'  => __( 'Secondary', 'global-styles' ),
+			'slug'  => 'secondary',
+			'color' => '#005177',
+		),
+	) );
+
+	add_theme_support( 'editor-gradient-presets', array(
+		array(
+			'name'     => __( 'Vivid cyan blue to vivid purple', 'global-styles' ),
+			'gradient' => 'linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%)',
+			'slug'     => 'vivid-cyan-blue-to-vivid-purple'
+		),
+		array(
+			'name'     => __( 'Vivid green cyan to vivid cyan blue', 'global-styles' ),
+			'gradient' => 'linear-gradient(135deg,rgba(0,208,132,1) 0%,rgba(6,147,227,1) 100%)',
+			'slug'     =>  'vivid-green-cyan-to-vivid-cyan-blue',
+		),
+	) );
+
+	add_theme_support( 'editor-font-sizes', array(
+		array(
+			'name' => __( 'Small', 'global-styles' ),
+			'size' => 12,
+			'slug' => 'small'
+		),
+		array(
+			'name' => __( 'Huge', 'global-styles' ),
+			'size' => 50,
+			'slug' => 'huge'
+		)
+	) );
+}
+add_action( 'after_setup_theme', 'global_styles_theme_after_setup' );
