51db1a4960ca5f081fc9ebf991a61f2e816d3b0f
Andrés <nosolosw@users.noreply.github.com>
2020-05-21 10:23:14 +0200

    Try/managed CSS (#5)

--- functions.php
+++ functions.php
@@ -1,14 +1,8 @@
 <?php
 
-function global_styles_experimental_after_setup_theme() {
-	add_theme_support('editor-styles');
-	add_editor_style('style.css');
-}
-add_action( 'after_setup_theme', 'global_styles_experimental_after_setup_theme' );
-
 function global_styles_experimental_wp_enqueue_scripts() {
 	wp_enqueue_style(
-		'global-styles-theme-style-resets',
+		'global-styles-theme-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		array(),
 		filemtime( dirname( __FILE__ ) . '/style.css' )
@@ -22,39 +16,44 @@ function global_styles_theme_after_setup() {
 		array(
 			'name'  => __( 'Primary', 'global-styles' ),
 			'slug'  => 'primary',
-			'color' => '#0073AA',
+			'color' => 'rgb(131, 12, 8)',
 		),
 		array(
 			'name'  => __( 'Secondary', 'global-styles' ),
 			'slug'  => 'secondary',
-			'color' => '#005177',
-		),
-	) );
-
-	add_theme_support( 'editor-gradient-presets', array(
-		array(
-			'name'     => __( 'Vivid cyan blue to vivid purple', 'global-styles' ),
-			'gradient' => 'linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%)',
-			'slug'     => 'vivid-cyan-blue-to-vivid-purple'
+			'color' => 'rgb(60, 12, 15)',
 		),
 		array(
-			'name'     => __( 'Vivid green cyan to vivid cyan blue', 'global-styles' ),
-			'gradient' => 'linear-gradient(135deg,rgba(0,208,132,1) 0%,rgba(6,147,227,1) 100%)',
-			'slug'     =>  'vivid-green-cyan-to-vivid-cyan-blue',
+			'name'  => __( 'Tertiary', 'global-styles' ),
+			'slug'  => 'tertiary',
+			'color' => 'rgb(209, 207, 203)',
+		),
+		array(
+			'name'  => __( 'Quaternary', 'global-styles' ),
+			'slug'  => 'quaternary',
+			'color' => 'rgb(30, 30, 30)',
 		),
 	) );
 
 	add_theme_support( 'editor-font-sizes', array(
 		array(
-			'name' => __( 'Small', 'global-styles' ),
-			'size' => 12,
-			'slug' => 'small'
+			'name' => __( 'Primary', 'global-styles' ),
+			'size' => 16,
+			'slug' => 'normal'
 		),
 		array(
-			'name' => __( 'Huge', 'global-styles' ),
-			'size' => 50,
+			'name' => __( 'Secondary', 'global-styles' ),
+			'size' => 32,
+			'slug' => 'big'
+		),
+		array(
+			'name' => __( 'Tertiary', 'global-styles' ),
+			'size' => 60,
 			'slug' => 'huge'
 		)
 	) );
+
+	add_theme_support('editor-styles');
+	add_editor_style('style.css');
 }
 add_action( 'after_setup_theme', 'global_styles_theme_after_setup' );
